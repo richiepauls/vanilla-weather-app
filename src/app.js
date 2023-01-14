dfe868eaa38e7a1a07ea8f7e3c69e456f2584b98
richiepauls <rachelanyebe@gmail.com>
2023-01-14 11:25:58 +0100

    js for temp, humidity and wind was fixed

---
+++ src/app.js
@@ -0,0 +1,16 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+let apiKey = "63214c4281922e3bb72fdf12dada7734";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New york&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
